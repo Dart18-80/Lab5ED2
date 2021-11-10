a10dc2925e6d484b923acd8f0e65a547ce39f19b
randres <randr@DESKTOP-DVJNG83>
2021-11-10 00:20:22 -0600

    nue

---
+++ Library_SDES/InterfazLlave.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library_SDES
+{
+    class InterfazLlave
+    {
+        public int LlavePrivada { get; set; }
+        public int LlavePublica { get; set; }
+
+    }
+}
